b280c6f7a1093f0149a2dda000b9a05def0654f3
bazibet11 <kiranarooj2346ws@gmail.com>
2026-04-23 20:36:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
-// Signup
-function signup() {
-  let user = document.getElementById("username").value;
-  let pass = document.getElementById("password").value;
+function loginUser(e) {
+  e.preventDefault();
 
-  localStorage.setItem("user", user);
-  localStorage.setItem("pass", pass);
-
-  alert("Signup Success");
-  window.location.href = "login.html";
-}
-
-// Login
-function login() {
   let user = document.getElementById("loginUser").value;
   let pass = document.getElementById("loginPass").value;
 
   let savedUser = localStorage.getItem("user");
   let savedPass = localStorage.getItem("pass");
 
-  if (user === savedUser && pass === savedPass) {
-    localStorage.setItem("loggedIn", user);
+  if(user === savedUser && pass === savedPass){
+    alert("Login Success");
     window.location.href = "dashboard.html";
   } else {
-    alert("Wrong details");
+    alert("Wrong Details");
   }
 }
 
-// Dashboard
-if (window.location.pathname.includes("dashboard.html")) {
-  let user = localStorage.getItem("loggedIn");
-  document.getElementById("welcome").innerText = "Welcome " + user;
-}
-
-// Game
-function startGame() {
-  alert("Game Start (next step me real game add hoga)");
+function goSignup(){
+  window.location.href = "signup.html";
 }
